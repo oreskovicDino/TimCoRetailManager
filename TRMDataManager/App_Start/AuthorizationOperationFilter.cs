3549d7a6ca34f03458b1ff3a46efbca2c80267f1
DevDooC <78646611+DevDooC@users.noreply.github.com>
2021-11-28 15:06:04 +0100

    Add Swagger Doc to API (#4)
    Adds Swashbuckle/Swagger to the API for documentation and testing.
    
    Co-authored-by: oreskovic.dino <oreskovic.dino@gmail.com>

---
+++ TRMDataManager/App_Start/AuthorizationOperationFilter.cs
@@ -0,0 +1,30 @@
+﻿namespace TRMDataManager.App_Start
+{
+    using Swashbuckle.Swagger;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Http.Description;
+
+    public class AuthorizationOperationFilter : IOperationFilter
+    {
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            if (operation.parameters == null)
+            {
+                operation.parameters = new List<Parameter>();
+                
+            }
+
+            operation.parameters.Add(new Parameter
+            {
+                name = "Authorization",
+                @in = "header",
+                description = "access token",
+                type = "string",
+                required = false
+            });
+        }
+    }
+}
