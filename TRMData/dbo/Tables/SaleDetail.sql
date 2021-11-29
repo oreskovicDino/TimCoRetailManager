3d7df702e6b91246914340c56f08a19cf1801095
oreskovic.dino <oreskovic.dino@gmail.com>
2021-11-29 18:18:03 +0100

    Register DB Tables
    The tables for the basic register system

---
+++ TRMData/dbo/Tables/SaleDetail.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[SaleDetail]
+(
+	[SaleId] INT NOT NULL PRIMARY KEY,
+	[ProductID] int not null, 
+    [Quantity] INT NOT NULL DEFAULT 1, 
+    [PurchasePrice] MONEY NOT NULL, 
+    [Tax] MONEY NOT NULL DEFAULT 0
+)
