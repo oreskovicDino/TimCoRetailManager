cb7c3f7a9870a240dcf10785aa1bf59ee6bc2a87
oreskovic.dino <oreskovic.dino@gmail.com>
2021-12-30 20:38:35 +0100

    Container Refrance Replacement
    Replace container reference with the IoC reference instead.

--- TRMDesktopUI/ViewModels/ShellViewModel.cs
+++ TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -1,27 +1,20 @@
 ﻿namespace TRMDesktopUI.ViewModels
 {
     using Caliburn.Micro;
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     using TRMDesktopUI.EventModels;
 
     public class ShellViewModel : Conductor<object>, IHandle<LogOnEvent>
     {
         private IEventAggregator @event;
         private SalesViewModel salsesVM;
-        private SimpleContainer container;
 
-        public ShellViewModel(IEventAggregator @event, SalesViewModel salsesVM, SimpleContainer container)
+        public ShellViewModel(IEventAggregator @event, SalesViewModel salsesVM)
         {
             this.salsesVM = salsesVM;
-            this.container = container;
             this.@event = @event;
             this.@event.Subscribe(this);
 
-            ActivateItem(this.container.GetInstance<LoginViewModel>());
+            ActivateItem(IoC.Get<LoginViewModel>());
         }
 
         public void Handle(LogOnEvent message)
