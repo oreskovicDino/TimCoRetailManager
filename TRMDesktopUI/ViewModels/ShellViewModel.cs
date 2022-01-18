40c7040ae1b40ac4a0705159765d4f6b851091a3
oreskovic.dino <oreskovic.dino@gmail.com>
2022-01-18 21:38:02 +0100

    Bug Fixes
    Fixes cart display bug, menu bug, and more

--- TRMDesktopUI/ViewModels/ShellViewModel.cs
+++ TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -1,16 +1,19 @@
 ﻿namespace TRMDesktopUI.ViewModels
 {
     using Caliburn.Micro;
+    using TRMDesktop.Library.Models;
     using TRMDesktopUI.EventModels;
 
     public class ShellViewModel : Conductor<object>, IHandle<LogOnEvent>
     {
         private IEventAggregator @event;
         private SalesViewModel salsesVM;
+        private ILoggedInUserModel user;
 
-        public ShellViewModel(IEventAggregator @event, SalesViewModel salsesVM)
+        public ShellViewModel(IEventAggregator @event, SalesViewModel salsesVM, ILoggedInUserModel user)
         {
             this.salsesVM = salsesVM;
+            this.user = user;
             this.@event = @event;
             this.@event.Subscribe(this);
 
@@ -20,6 +23,40 @@
         public void Handle(LogOnEvent message)
         {
             ActivateItem(salsesVM);
+            NotifyOfPropertyChange(() => IsLoggedIn);
+        }
+
+        public void ExitApplication()
+        {
+            TryClose();
+        }
+        private bool isLoggedIn;
+
+        public bool IsLoggedIn
+        {
+            get
+            {
+                bool output = false;
+
+                if (string.IsNullOrWhiteSpace(user.Token) == false )
+                {
+                    output = true;
+                }
+
+                return output;
+            }
+            set
+            {
+                NotifyOfPropertyChange(() => IsLoggedIn);
+                isLoggedIn = value;
+            }
+        }
+
+        public void LogOut()
+        {
+            user.LogOffUser();
+            ActivateItem(IoC.Get<LoginViewModel>());
+            NotifyOfPropertyChange(() => IsLoggedIn);
         }
     }
 }
