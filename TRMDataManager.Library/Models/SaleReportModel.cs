e6858b74ac48c1e0f1dd7b10f719891b8e281788
Dino Orešković <oreskovic.dino@gmail.com>
2022-01-22 11:55:43 +0100

    Add Admin Endpoint Data (#22)
    Adds the data for administrative endpoints.

---
+++ TRMDataManager.Library/Models/SaleReportModel.cs
@@ -0,0 +1,15 @@
+﻿namespace TRMDataManager.Library.Models
+{
+    using System;
+
+    public class SaleReportModel
+    {
+        public DateTime SaleDate { get; set; }
+        public decimal SubTotal { get; set; }
+        public decimal Tax { get; set; }
+        public decimal Total { get; set; }
+        public string FristName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
